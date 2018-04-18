c3ac988108f0e9e963fe38df5dfd062729f2a0f4
Lukundu Lako <lukundu.lako@gmail.com>
2018-04-18 18:24:00 -0400

    initial push

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class Teleport : MonoBehaviour {
+	float mfieldOfView;
+	//public Camera camera;
+
+    private Camera sceneCamera;
+    private Camera playerCamera;
+    private networkManager networkmgn; 
+	private Button EmojiBtn;
+	private Button ExitBtn;
+	private GameObject title;
+
+
+	// Use this for initialization
+	void Start () {
+		title = GameObject.Find ("ActiveChats");
+		EmojiBtn = GameObject.Find ("EmojiBtn").GetComponent<Button>();
+		ExitBtn = GameObject.Find ("ExitBtn").GetComponent<Button>();
+		mfieldOfView = 60.0f;
+        sceneCamera = Camera.main;
+        playerCamera = gameObject.GetComponentInChildren<Camera>();
+        networkmgn = new networkManager(); 
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+		RaycastHit hit;
+		Ray ray =playerCamera.ScreenPointToRay(Input.mousePosition);
+		//sceneCamera = Camera.main;
+
+        //sceneCamera.fieldOfView = mfieldOfView;
+
+		if (Input.GetMouseButtonDown (0)) {
+
+			if (Physics.Raycast (ray, out hit)) {
+				Transform objectHit = hit.transform;
+
+				if (objectHit.gameObject.tag == "chat") {
+
+					title .SetActive (false);
+
+					EmojiBtn.interactable = true;
+					ExitBtn.interactable = true;
+
+                    networkmgn.OnClick(new Vector3(objectHit.position.x, -18f, objectHit.position.z));
+                    gameObject.transform.position = new Vector3 (objectHit.position.x, -18f, objectHit.position.z);
+                    ToggleCameraON();
+                }
+			}
+		}
+
+
+		if (Input.GetKey(KeyCode.Escape)){
+
+           // ToggleCameraOFF();
+			EmojiBtn.interactable = false;
+			ExitBtn.interactable = false;
+			title .SetActive (true);
+
+          	gameObject.transform.position = new Vector3 (0, 0, 0);
+			gameObject.transform.eulerAngles = new Vector3 (0,  0f, 0);
+
+		}
+	}
+    //onclick change camera between scene and FPV
+    public void ToggleCameraON() {
+//         sceneCamera.enabled = false;
+//         playerCamera.enabled = true;
+
+    }
+    public void ToggleCameraOFF()
+    {
+//        sceneCamera.enabled = true;
+//        playerCamera.enabled = false;
+
+    }
+
+}
---
+++ Assets/Scripts/updateEmot.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class updateEmot : MonoBehaviour {
+    public Material mHappy;
+    public Material mFunny;
+    public Material mTongue;
+    public Material mWink;
+    public Material mLove;
+    public Material mShy;
+	public int activeEmoji;
+	private Camera playerCamera;
+	public ParticleSystem particleShy, particleLove, particleLaugh, particleTongue, particleWink, particleHappy;
+
+    private PhotonView photonView;
+    // Use this for initialization
+    void Start () {
+		activeEmoji = 1;
+        photonView = GetComponent<PhotonView>();
+        PhotonNetwork.sendRate = 60;
+        PhotonNetwork.sendRateOnSerialize = 1;
+		playerCamera = gameObject.GetComponentInChildren<Camera>();
+		particleShy = GameObject.Find ("ParticleShy").GetComponent<ParticleSystem>();
+		particleLove = GameObject.Find ("ParticleLove").GetComponent<ParticleSystem>();
+		particleLaugh = GameObject.Find ("ParticleLaugh").GetComponent<ParticleSystem>();
+		particleTongue = GameObject.Find ("ParticleTongue").GetComponent<ParticleSystem>();
+		particleWink = GameObject.Find ("ParticleWink").GetComponent<ParticleSystem>();
+		particleHappy = GameObject.Find ("ParticleWink").GetComponent<ParticleSystem>();
+
+    }
+
+	public void setActiveEmoji (int emoji) {
+		activeEmoji = emoji;
+		Debug.Log ("it worked");
+	}
+		
+    // Update is called once per frame
+    void Update()
+    {
+		
+        if (!photonView.isMine)
+        {
+            return;
+        }
+	
+
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			
+			GetComponentInChildren<Renderer> ().material = mHappy;
+			particleHappy.Play (); 
+			particleHappy.Emit (10);
+			Debug.Log ("_1");
+			photonView.RPC ("SetEmot", PhotonTargets.All, "mHappy");
+		} else if (Input.GetKeyDown (KeyCode.Alpha2))  {
+			particleShy.Play ();
+			particleShy.Emit (10);
+			GetComponentInChildren<Renderer> ().material = mShy;
+			photonView.RPC ("SetEmot", PhotonTargets.All, "mShy");
+		} else if (Input.GetKeyDown (KeyCode.Alpha6))  {
+			particleTongue.Play ();
+			particleTongue.Emit (10);
+			GetComponentInChildren<Renderer> ().material = mTongue;
+			photonView.RPC ("SetEmot", PhotonTargets.All, "mTongue");
+		} else if (Input.GetKeyDown (KeyCode.Alpha3)) {
+			particleWink.Play ();
+			particleWink.Emit (10);
+			GetComponentInChildren<Renderer> ().material = mWink;
+			photonView.RPC ("SetEmot", PhotonTargets.All, "mWink");
+		} else if (Input.GetKeyDown (KeyCode.Alpha4))  {
+			particleLove.Play ();
+			particleLove.Emit (10);
+			GetComponentInChildren<Renderer> ().material = mLove;
+			photonView.RPC ("SetEmot", PhotonTargets.All, "mLove");
+		} else if (Input.GetKeyDown (KeyCode.Alpha5)) {
+			particleLaugh.Play ();
+			particleLaugh.Emit (10);
+			GetComponentInChildren<Renderer> ().material = mFunny;
+			photonView.RPC ("SetEmot", PhotonTargets.All, "mFunny");
+		} else {
+			particleHappy.Stop ();
+			particleShy.Stop ();
+			particleWink.Stop ();
+			particleLove.Stop ();
+			particleTongue.Stop ();
+			particleLaugh.Stop ();
+
+
+		}
+    }
+    [PunRPC]
+    void SetEmot(string face)
+    {
+        Debug.Log(face);
+        switch (face) {
+
+            case "mFunny":
+                StartCoroutine(UpdateEmot(mFunny));
+                break;
+            case "mTongue":
+                StartCoroutine(UpdateEmot(mTongue));
+                break;
+            case "mWink":
+                StartCoroutine(UpdateEmot(mWink));
+                break;
+            case "mLove":
+                StartCoroutine(UpdateEmot(mLove));
+                break;
+            default:
+                break;
+
+
+        }
+        
+    }
+
+    IEnumerator UpdateEmot(Material face) {
+        gameObject.GetComponentInChildren<MeshRenderer>().material = face; 
+        yield return new WaitForSeconds(3);
+        gameObject.GetComponentInChildren<MeshRenderer>().material = mHappy;
+
+    }
+}
